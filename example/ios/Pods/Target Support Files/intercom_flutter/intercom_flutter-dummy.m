eb3fb3923433284944732672e8b7aaaeab16e5f6
Confidence Wangoho <confidencewangoho@Confidences-MacBook-Pro.local>
2022-09-20 20:05:54 +0100

    first commit

---
+++ example/ios/Pods/Target Support Files/intercom_flutter/intercom_flutter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_intercom_flutter : NSObject
+@end
+@implementation PodsDummy_intercom_flutter
+@end
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<intercom_flutter/IntercomFlutterPlugin.h>)
+#import <intercom_flutter/IntercomFlutterPlugin.h>
+#else
+@import intercom_flutter;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [IntercomFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"IntercomFlutterPlugin"]];
+}
+
+@end
